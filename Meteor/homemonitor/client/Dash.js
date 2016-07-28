eac659eb837b23a1b5c482a1e1acfcf9861f9549
CGodoi <yarock@gmail.com>
2016-07-27 21:17:19 -0300

    version desarrollo
    revisar

--- Meteor/homemonitor/client/Dash.js
+++ Meteor/homemonitor/client/Dash.js
@@ -18,7 +18,14 @@ Template.Dash.helpers({
 	},
 	sectores:()=>{
 		return Sectores.find();
-	}
+	},
+	compare:function(v1, v2) {
+  		if (typeof v1 === 'object' && typeof v2 === 'object') {
+    		return _.isEqual(v1, v2); // do a object comparison
+  		} else {
+    		return v1 === v2;
+	    }
+	}    
 });
 
 
